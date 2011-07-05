3bacbab7863be4b8b58eaa255b71c0854daccef0
Vladislav Klimanov <ahmpro@rslayers.com>
2011-07-05 09:37:59 +0400

    Каркас готов.

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+#include "task02.h"
+
+int Task[32] = {0};
+void (*Func[32])();
+
 int main(){
+    int n = 0;
+    Task[2] = 1; Func[2] = task02;
+
+    printf("Select Task: ");
+    scanf_s("%d", &n);
+
+    if ( (n<1) & (n>31) )
+    { printf("Incorrect task"); }
+    else
+    {
+        if (!Task[n])
+        { printf("Task is unavailable\n"); }
+        else
+        {
+            Func[n]();
+        }
+    }
+
 	return 0;
 }
---
+++ task02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void task02()
+{
+    printf("It's task02() in Task02.c\n");
+}
