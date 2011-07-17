277146baca07ae4b5e3ea42e776857f075a059e7
Vladislav Klimanov <ahmpro@rslayers.com>
2011-07-17 19:37:36 +0400

    Добавил 24 задание и отрефакторил main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include "task02.h"
 #include "task05.h"
+#include "task24.h"
 
 int Task[32] = {0};
 void (*Func[32])();
@@ -10,21 +11,15 @@ int main(){
     int n = 0;
     Task[2] = 1; Func[2] = task02;
     Task[5] = 1; Func[5] = task05;
+    Task[24] = 1; Func[24] = task24;
 
     printf("Select Task: ");
     scanf_s("%d", &n);
 
-    if ( (n<1) & (n>31) )
-    { printf("Incorrect task"); }
+    if ( (n<1) & (n>31) | !Task[n])
+        printf("Task incorrect or unavialable");
     else
-    {
-        if (!Task[n])
-        { printf("Task is unavailable\n"); }
-        else
-        {
-            Func[n]();
-        }
-    }
+        Func[n];
 
 	return 0;
 }
